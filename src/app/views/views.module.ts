01f5fd330cbf618a0c34f2b496e203c1f6888b9e
SantamariaD <diego-oz@hotmail.com>
2021-12-14 22:59:34 -0600

    Primera estructura de carpetas de la aplicación

---
+++ src/app/views/views.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ProductsComponent } from './products/products.component';
+
+
+
+@NgModule({
+  declarations: [
+    ContactComponent,
+    HomeComponent,
+    LoginComponent,
+    ProductsComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ViewsModule { }
